79bf30faeedaf5f56760846d27fa354a7f0c83c5
Mostafa Hadian <mostafa.hadian@karhoo.com>
2020-10-15 16:19:32 +0100

    Update ExampleInstrumentedTest.kt

--- ui-sdk-components-sample/app/src/androidTest/java/com/karhoo/samples/uisdk/components/ExampleInstrumentedTest.kt
+++ ui-sdk-components-sample/app/src/androidTest/java/com/karhoo/samples/uisdk/components/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.karhoo.samples.networksdk", appContext.packageName)
+        assertEquals("com.karhoo.samples.uisdk.components", appContext.packageName)
     }
 }
