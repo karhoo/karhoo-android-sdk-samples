0d549374fcc279f8b658d454bc5cb5d32b1f7940
EdwardW2 <44901854+EdwardW2@users.noreply.github.com>
2020-11-11 16:09:49 +0000

    Booking now works

--- ui-sdk-components-sample/app/src/main/java/com/karhoo/samples/uisdk/components/MainActivity.kt
+++ ui-sdk-components-sample/app/src/main/java/com/karhoo/samples/uisdk/components/MainActivity.kt
@@ -64,6 +64,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        for(fragment in supportFragmentManager.fragments) {
+            fragment.onActivityResult(requestCode, resultCode, data)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
