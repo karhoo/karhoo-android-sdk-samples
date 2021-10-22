f0683da9aad20b134963167786c193bac1159d0f
Mostafa Hadian <mostafa.hadian@karhoo.com>
2021-10-22 12:47:38 +0100

    update colors

--- ui-sdk-dropin-sample/app/src/main/java/com/karhoo/samples/uisdk/dropin/MainActivity.kt
+++ ui-sdk-dropin-sample/app/src/main/java/com/karhoo/samples/uisdk/dropin/MainActivity.kt
@@ -9,13 +9,24 @@ import com.karhoo.samples.uisdk.dropin.config.GuestConfig
 import com.karhoo.samples.uisdk.dropin.config.TokenExchangeConfig
 import com.karhoo.sdk.api.KarhooApi
 import com.karhoo.sdk.api.KarhooError
+import com.karhoo.sdk.api.model.Position
+import com.karhoo.sdk.api.network.request.PassengerDetails
 import com.karhoo.sdk.api.network.response.Resource
 import com.karhoo.uisdk.KarhooUISDK
 import com.karhoo.uisdk.screen.booking.BookingActivity
+import com.karhoo.uisdk.screen.booking.domain.address.JourneyInfo
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var loadingProgressBar: View
+
+    val passengerDetails = PassengerDetails(firstName = "Passenger",
+        lastName = "Details", email = "passenger+email@karhoo.com",
+        phoneNumber = "+15005550006", locale = "en")
+    val journeyInfo = JourneyInfo(
+        origin = Position(latitude = 51.5166777, longitude = -0.1791215),
+        destination = Position(latitude = 51.5010249, longitude = -0.1268513), date = null
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +44,36 @@ class MainActivity : AppCompatActivity() {
             goToBooking()
         }
 
+        findViewById<Button>(R.id.bookTripButtonGuestWithPassenger).setOnClickListener {
+            showLoading()
+
+            applyGuestConfig()
+
+            goToBookingWithPassenger()
+        }
+
+        findViewById<Button>(R.id.bookTripButtonGuestWithPassengerWithJourney).setOnClickListener {
+            showLoading()
+
+            applyGuestConfig()
+
+            goToBookingWithPassengerAndJourney()
+        }
+
         findViewById<Button>(R.id.bookTripButtonTokenExchange).setOnClickListener {
             showLoading()
 
             applyTokenExchangeConfig()
 
-            loginTokenExchange()
+            loginTokenExchange(withPassenger = false)
+        }
+
+        findViewById<Button>(R.id.bookTripButtonTokenExchangeWithPassenger).setOnClickListener {
+            showLoading()
+
+            applyTokenExchangeConfig()
+
+            loginTokenExchange(withPassenger = true)
         }
     }
 
@@ -62,12 +97,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loginTokenExchange() {
+    private fun loginTokenExchange(withPassenger: Boolean) {
         KarhooApi.userService.logout()
         val token: String = BuildConfig.BRAINTREE_AUTH_TOKEN
         KarhooApi.authService.login(token).execute { result ->
             when (result) {
-                is Resource.Success -> goToBooking()
+                is Resource.Success -> {
+                    if (withPassenger) {
+                        goToBookingWithPassenger()
+                    } else {
+                        goToBooking()
+                    }
+                }
                 is Resource.Failure -> toastErrorMessage(result.error)
             }
         }
@@ -77,6 +118,27 @@ class MainActivity : AppCompatActivity() {
         val builder = BookingActivity.Builder.builder
             .initialLocation(null)
         startActivity(builder.build(this))
+        hideLoading()
+    }
+
+    private fun goToBookingWithPassengerAndJourney() {
+        val builder = BookingActivity.Builder.builder
+            .initialLocation(null)
+        startActivity(builder
+            .journeyInfo(journeyInfo)
+            .passengerDetails(passengerDetails)
+            .comments("Some comment")
+            .build(this))
+        hideLoading()
+    }
+
+    private fun goToBookingWithPassenger() {
+        val builder = BookingActivity.Builder.builder
+            .initialLocation(null)
+        startActivity(builder
+            .passengerDetails(passengerDetails)
+            .comments("Some comment")
+            .build(this))
         hideLoading()
     }
 
